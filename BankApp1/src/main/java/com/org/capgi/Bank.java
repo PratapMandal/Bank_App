d03ea69f576959aee95d2c058aa52ab4fd050965
Radhe <Radhe@DESKTOP-D8RM4MV>
2020-04-01 23:18:21 -0700

    Commited

---
+++ BankApp1/src/main/java/com/org/capgi/Bank.java
@@ -0,0 +1,66 @@
+
+package com.org.capgi;
+import java.util.Scanner;
+
+public class Bank {
+	
+	private String accno;
+	private String name;
+	private long balance;
+
+	Scanner KB=new Scanner(System.in);
+	
+	//method to open an account
+	public void openAccount()
+	{ 
+		System.out.print("Enter Account No: ");
+		accno=KB.next();
+		System.out.print("Enter Name: ");
+		name=KB.next();
+		System.out.print("Enter Balance: ");
+		balance=KB.nextLong();
+	}
+
+	//method to display account details
+	public void showAccount()
+	{ 
+		System.out.println(accno+","+name+","+balance);
+	}
+
+	//method to deposit money
+	public void deposit()
+	{
+		long amt;
+		System.out.println("Enter Amount U Want to Deposit : ");
+		amt=KB.nextLong();
+		balance=balance+amt;
+	}
+
+	//method to withdraw money
+	public void withdrawal()
+	{
+		long amt;
+		System.out.println("Enter Amount U Want to withdraw : ");
+		amt=KB.nextLong();
+		if(balance>=amt)
+		{ 
+			balance=balance-amt;
+		}
+		else
+		{
+			System.out.println("Less Balance..Transaction Failed..");
+		}
+	}
+
+	//method to search an account number
+	public boolean search(String acn)
+	{ 
+		if(accno.equals(acn))
+		{ 
+			showAccount();
+			return(true);
+		}
+		return(false);
+	}
+}
+
